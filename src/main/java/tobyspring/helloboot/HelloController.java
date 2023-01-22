69f4731694ad1b844d740b883cdb86c53d9b028a
wjrdjs005 <wjrdjs005@naver.com>
2023-01-22 14:23:42 +0900

    서블릿컨텍스트를디스패치서블릿으로변경

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,12 +1,24 @@
 package tobyspring.helloboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
+@RequestMapping
 public class HelloController {
 
+    private final HelloService helloService;
+
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
+    @GetMapping("/hello")
+    @ResponseBody
     public String hello(String name) {
-        return "hello " + name;
+        return helloService.sayHello(Objects.requireNonNull(name));
     }
 
 }
